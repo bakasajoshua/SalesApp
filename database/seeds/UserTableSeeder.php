b440bca8d2ffd82d62b1e0c1694fa2cf5bae77e5
Shadrack <shadrackbakasa5@gmail.com>
2019-06-15 12:10:07 +0300

    Creating and seeding user_type table

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -31,6 +31,18 @@ class UserTableSeeder extends Seeder
         	'user_type_id' => 1,
         	'email_verified_at' => date('Y-m-d H:i:s')
         ]);
+
+        factory('App\Usertype', 1)->create([
+        	'user_type' => 'Manager',
+        ]);
+
+        factory('App\Usertype', 1)->create([
+        	'user_type' => 'Supervisor',
+        ]);
+
+        factory('App\Usertype', 1)->create([
+        	'user_type' => 'Sales Rep',
+        ]);
     }
     
 }
